d8449ea519a0fbdc7925e4adde45707381809d0a
Santiago Álvarez <santiago.alvarezb@upb.edu.co>
2021-08-29 17:11:56 -0500

    Creación "laberinto" xd

--- Parte 2/Parte 2/SearchPath.cs
+++ Parte 2/Parte 2/SearchPath.cs
@@ -32,6 +32,48 @@ namespace Parte_2
     }
     class SearchPath
     {
+        private static Node startingPoint = new Node(0, 0);
+        private static Node endingPoint = new Node(2, -2);
+        private static Node searchingPoint;
+        private static bool isExploring = true;                       // If we are end then it is set to false
+
+        private static List<Node> path = new List<Node>();            // For storing the path traversed
+
         private static Dictionary<Vector, Node> block = new Dictionary<Vector, Node>();
+        private static Queue<Node> queue = new Queue<Node>();
+
+        static int[,] maze = { { 0, 0, 0 }, { 0, 1, 0 }, { 0, 0, 0 }, { 0, 0, 1 }};
+        static int[][] directions = new int[4][];
+
+        public static void CreateMaze()
+        {
+            block.Add(new Vector(0, 0), startingPoint);
+            block.Add(new Vector(1,0), new Node(1, 0)); 
+          //block.Add(new Vector(2, 0), new Node(2, 0));
+            block.Add(new Vector(0, -1), new Node(0, -1));
+            //block.Add(new Vector(1, -1), new Node(1, -1));
+            block.Add(new Vector(2, -1), new Node(2, -1));
+            block.Add(new Vector(0, -2), new Node(0, -2));
+            block.Add(new Vector(1, -2), new Node(1, -2));
+            block.Add(new Vector(2, -2), new Node(2, -2));
+            //block.Add(new Vector(0, -3), new Node(0, -3));
+            block.Add(new Vector(1, -3), endingPoint);
+            //block.Add(new Vector(2, -3), new Node(2, -3));
+
+        }
+
+        public static void BFS()
+        {
+            queue.Enqueue(startingPoint);
+            while (queue.Count > 0 && isExploring)
+            {
+                searchingPoint = queue.Dequeue();
+               // OnReachingEnd();
+                //ExploreNeighbourNodes();
+
+            }
+        }
+
+
     }
 }
