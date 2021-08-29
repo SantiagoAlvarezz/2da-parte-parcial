0474008d20253164ac69787ac91673806a832e6e
Santiago Álvarez <santiago.alvarezb@upb.edu.co>
2021-08-29 16:55:40 -0500

    Nodos xd

--- Parte 2/Parte 2/SearchPath.cs
+++ Parte 2/Parte 2/SearchPath.cs
@@ -4,7 +4,34 @@ using System.Text;
 
 namespace Parte_2
 {
+
+    class Node
+    {
+        public bool isExplored = false;
+        public Node isExploredFrom;
+
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Node(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+    struct Vector
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Vector(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
     class SearchPath
     {
+        private static Dictionary<Vector, Node> block = new Dictionary<Vector, Node>();
     }
 }
