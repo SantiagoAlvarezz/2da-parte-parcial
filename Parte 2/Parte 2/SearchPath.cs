77e7e6c5654237988a51a75aeeb39abce7f40ef9
Santiago Álvarez <santiago.alvarezb@upb.edu.co>
2021-08-29 19:14:33 -0500

    Correción laberinto y finalización del programa xd

--- Parte 2/Parte 2/SearchPath.cs
+++ Parte 2/Parte 2/SearchPath.cs
@@ -33,7 +33,7 @@ namespace Parte_2
     class SearchPath
     {
         private static Node startingPoint = new Node(0, 0);
-        private static Node endingPoint = new Node(2, -2);
+        private static Node endingPoint = new Node(1, -3);
         private static Node searchingPoint;
         private static bool isExploring = true;                       // If we are end then it is set to false
 
@@ -42,7 +42,7 @@ namespace Parte_2
         private static Dictionary<Vector, Node> block = new Dictionary<Vector, Node>();
         private static Queue<Node> queue = new Queue<Node>();
 
-        static int[,] maze = { { 0, 0, 0 }, { 0, 1, 0 }, { 0, 0, 0 }, { 0, 0, 1 }};
+        static int[,] maze = { { 0, 0, 1 }, { 0, 1, 0 }, { 0, 0, 0 }, { 1, 0, 1 }};
         static int[][] directions = new int[4][];
 
         public static void CreateMaze()
@@ -62,18 +62,102 @@ namespace Parte_2
 
         }
 
+      
         public static void BFS()
         {
             queue.Enqueue(startingPoint);
             while (queue.Count > 0 && isExploring)
             {
                 searchingPoint = queue.Dequeue();
-               // OnReachingEnd();
-                //ExploreNeighbourNodes();
+                OnReachingEnd();
+                ExploreNeighbourNodes();
 
             }
+        }
+        static void ExploreNeighbourNodes()
+        {
+            if (!isExploring) { return; }
+
+            directions[0] = new int[] { 0, -1 };
+            directions[1] = new int[] { 0, 1 };
+            directions[2] = new int[] { 1, 0 };
+            directions[3] = new int[] { -1, 0 };
+
+            for (int i = 0; i < 4; i++)
+            {
+                Vector neighbourPos = new Vector(searchingPoint.X + directions[i][0], searchingPoint.Y + directions[i][1]);
+
+                if (block.ContainsKey(neighbourPos))
+                {
+                    Node node = block[neighbourPos];
+                    if (!node.isExplored)
+                    {
+                        queue.Enqueue(node);                       // Enqueueing the node at this position
+                        node.isExplored = true;
+                        node.isExploredFrom = searchingPoint;      // Set how we reached the neighbouring node i.e the previous node; for getting the path
+                    }
+                }
+
+            }
+        }
+        private static void OnReachingEnd()
+        {
+            if (searchingPoint == endingPoint)
+            {
+                isExploring = false;
+            }
+            else
+            {
+                isExploring = true;
+            }
+        }
+
+        public static void CreatePath()
+        {
+            SetPath(endingPoint);
+            Node previousNode = endingPoint.isExploredFrom;
+
+            while (previousNode != startingPoint)
+            {
+                SetPath(previousNode);
+                previousNode = previousNode.isExploredFrom;
+            }
+
+            SetPath(startingPoint);
+            path.Reverse();
+
+        }
+        private static void SetPath(Node node)
+        {
+            path.Add(node);
+        }
+
+
+        public static void Imprimir()
+        {
+            Console.WriteLine("Laberinto:"); 
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write("[" + maze[i, j] + "]");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Camino solución:");
+            foreach (var i in path)
+            {
+                Console.WriteLine(i.X + "," + i.Y);
+
+            }
+            Console.WriteLine();
+
         }
 
 
     }
+
 }
+
